5d56cea305160d6c8287958be00985d2a2c2b01d
dewwanjana2023 <138287233+dewwanjana2023@users.noreply.github.com>
2024-09-17 22:46:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "if5XnLjK#twkfKRWevVeTplK6Mjd9RquCvPGsczT4WohnE5d8SO8",
+SESSION_ID: process.env.SESSION_ID || "eSxmyIbb#Ebg0lzXemR8Z6WvLI0peqZhskmlho9EjyMQmsY47Bz8",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
-SUDO_NB: process.env.SUDO_NB || "94741140620",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi Dᴇᴡ-Mᴅ Is Online Now 💻\n*💻 Owner* - Dᴇᴡ-MD\n\n*💻 Owner Number* -94760683496",
+SUDO_NB: process.env.SUDO_NB || "94778838273",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
